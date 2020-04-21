4382d37eb24cdd9a44bf0cd545aebe5b77056a0d
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-21 01:07:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,5 +5,15 @@ def loop_message_five_times(message)
   puts message
   puts message
   puts message
-  
+end
+
+def loop_message_n_times(message, number)
+  i = 0
+
+  while i <= number
+    puts message
+    i++
+
+  end
+
 end
